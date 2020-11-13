4b45da80bddbfed65be7fbcb1dd00562b5036ecc
Abhimanyu Hans <abhimanyu.hans30@gmail.com>
2020-11-14 00:57:59 +0530

    moved files

--- forms.py
+++ forms.py
@@ -1,11 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.fields.simple import PasswordField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.fields.core import BooleanField
+from wtforms.fields.simple import PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min = 2, max = 20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password',  validators=[DataRequired])
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired, EqualTo('password')])
+    ad_id = StringField('AD ID', validators=[DataRequired(), Length(min = 2, max = 20)])
+    password = PasswordField('Password',  validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign up')
+
+class LoginForm(FlaskForm):
+    ad_id = StringField('Username', validators=[DataRequired(), Length(min = 2, max = 20)])
+    password = PasswordField('Password',  validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- validation_app.py
+++ validation_app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
+from forms import RegistraionForm, LoginForm
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = '40702441b4d60a037091caa7349a0dac'
 
 scorecards = [
     {
@@ -31,6 +33,16 @@ def home():
 @app.route("/about")
 def about():
     return render_template("about.html", title = "About Us")
+
+@app.route("/register")
+def register():
+    form = RegistraionForm()
+    return render_template("register.html", title = "Registration", form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template("login.html", title = "Login", form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
